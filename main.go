98e6d6df6dd06fcec9e33ed10483475be5480ceb
oponcefranco <oleg.poncefranco@goat.com>
2021-08-12 12:44:04 -0700

    add middleware logger to capture response info

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
@@ -9,11 +10,72 @@ import (
 	"mime/multipart"
 	"net/http"
 	"os"
+	"time"
 )
 
 var listenAddr string
 
-func patchUpdateCsvFile(w http.ResponseWriter, r *http.Request) {
+type LogResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+	buf        bytes.Buffer
+}
+
+func NewLogResponseWriter(w http.ResponseWriter) *LogResponseWriter {
+	return &LogResponseWriter{ResponseWriter: w}
+}
+
+func (w *LogResponseWriter) WriteHeader(code int) {
+	w.statusCode = code
+	w.ResponseWriter.WriteHeader(code)
+}
+
+func (w *LogResponseWriter) Write(body []byte) (int, error) {
+	w.buf.Write(body)
+	return w.ResponseWriter.Write(body)
+}
+
+type LogMiddleware struct {
+	logger *log.Logger
+}
+
+func NewLogMiddleware(logger *log.Logger) *LogMiddleware {
+	return &LogMiddleware{logger: logger}
+}
+
+func (m *LogMiddleware) Func() mux.MiddlewareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			startTime := time.Now()
+
+			logRespWriter := NewLogResponseWriter(w)
+			next.ServeHTTP(logRespWriter, r)
+
+			m.logger.Printf(
+				"duration=%s status=%d body=%s",
+				time.Since(startTime).String(),
+				logRespWriter.statusCode,
+				logRespWriter.buf.String())
+		})
+	}
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = fmt.Fprintf(w, "Welcome Home!")
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = fmt.Fprintf(w, "Healthy!")
+}
+
+func CsvFileHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	parseCsvFile(w, r)
+}
+
+func parseCsvFile(w http.ResponseWriter, r *http.Request) {
 	// constructor.io accepts the following:
 	// r.FormFile("items")  or r.FormFile("variations")
 	file, handler, err := r.FormFile("variations")
@@ -37,31 +99,58 @@ func patchUpdateCsvFile(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 		}
 	}(f)
-	message := "PATCH request w/ file "+fileName+".csv was sent successfully"
+	message := "PATCH request was sent successfully: " + fileName + ".csv"
 	_, _ = io.WriteString(w, message)
 	_, _ = io.Copy(f, file)
-	logger := log.New(os.Stdout, "API Request: ", log.LstdFlags)
-	logger.Println(message)
 }
 
-func getIndex(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "Welcome Home!")
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
+	logger := log.New(os.Stdout, "", log.Lmicroseconds)
+
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration the server gracefully waits for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
 	flag.StringVar(&listenAddr, "listen-addr", ":8080", "server listen address")
 	flag.Parse()
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-	logger.Println("\nServer is starting on port"+listenAddr+"...")
+
+	logger.Println("\nServer is starting on port" + listenAddr + "...")
 
 	router := mux.NewRouter().StrictSlash(true)
+	router.Use(loggingMiddleware)
+
+	srv := &http.Server{
+		Addr:         "127.0.0.1:8080",
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
 	catalog := router.PathPrefix("/v1/catalog").Subrouter()
 
-	catalog.Path("").Methods(http.MethodGet).HandlerFunc(getIndex)
-	catalog.Path("").Methods(http.MethodPatch).HandlerFunc(patchUpdateCsvFile)
+	catalog.Path("/healthcheck").Methods(http.MethodGet).HandlerFunc(HealthCheckHandler)
+	catalog.Path("").Methods(http.MethodGet).HandlerFunc(IndexHandler)
+	catalog.Path("").Methods(http.MethodPatch).HandlerFunc(CsvFileHandler)
 
-	router.HandleFunc("/", getIndex).Methods("GET")
-	router.HandleFunc("/file", patchUpdateCsvFile).Methods("PATCH")
+	router.HandleFunc("/", IndexHandler).Methods("GET")
+	router.HandleFunc("/healthcheck", HealthCheckHandler).Methods("GET")
+	router.HandleFunc("/", CsvFileHandler).Methods("PATCH")
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	logMiddleware := NewLogMiddleware(logger)
+	router.Use(logMiddleware.Func())
+
+	logger.Fatalln(http.ListenAndServe(":8080", router))
 }
